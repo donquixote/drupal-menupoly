03628715425bfd3cdc1d7db4fa3c35c02133a06b
Andreas Hennings <andreas@dqxtech.net>
2013-02-05 05:10:22 +0100

    make processSubmenuItems() required in menu theme

--- lib/MenuTheme/Interface.php
+++ lib/MenuTheme/Interface.php
@@ -3,6 +3,8 @@
 
 interface menupoly_MenuTheme_Interface {
 
+  function processSubmenuItems(&$items);
+
   function renderMenuItem($item, $options, $submenu_html);
 
   function renderMenuItem__no_access($item, $options, $submenu_html);
